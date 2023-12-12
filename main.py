3b5f63505e37fd14691573721cad36e8e09dde5a
Ruslan-Far <rfarhetdinov@mail.ru>
2023-12-12 22:50:22 +0300

    начал процесс разработки подходящей архитектуры

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pybricks.ev3devices import (Motor, ColorSensor)
 from pybricks.parameters import Port, Direction, Color
 from pybricks.tools import wait
 
+# параметры для работы с цветами
 BLACK = 0
 BLUE = 1
 GREEN = 2
@@ -21,21 +22,96 @@ blue_h = [170, red_h[0] - 1]
 green_h = [80, blue_h[0] - 1]
 yellow_h = [40, green_h[0] - 1]
 red_h_2 = [0, yellow_h[0] - 1]
+# ----------------------------------------------------------
 
+# инициализация девайсов
 ev3 = EV3Brick()
-speed = 100
-angle_a = 160 # full 480
-angle_d = 195 # full 780
-motor_a = Motor(Port.A, Direction.COUNTERCLOCKWISE)
-motor_d = Motor(Port.D, Direction.COUNTERCLOCKWISE)
-
+MOTOR_A = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+MOTOR_D = Motor(Port.D, Direction.COUNTERCLOCKWISE)
 color_sensor = ColorSensor(Port.S1)
+# ----------------------------------------------------------
+
+ROWS = 5
+COLS = 4
+SPEED = 300
+
+# инициализация текущего состояния
+current_row = 0
+current_col = 0
+current_x = 0 # mm
+current_y = 0 # mm
+current_link_1_d = 0
+current_link_2_d = 0
+# ----------------------------------------------------------
+
+map_red_field = []
+
+class Point:
+	link_0_alpha = 90 # degrees
+	link_0_a = 0 # mm
+	link_1_theta = 0 # degrees
+	link_1_alpha = 90 # degrees
+	link_1_a = 122 # mm
+	link_2_theta = 0 # degrees
+
+	def __init__(self, x, y, link_1_d, link_2_d):
+		self.x = x
+		self.y = y
+		self.link_1_d = link_1_d
+		self.link_2_d = link_2_d
+
+
+def init_map():
+	row = current_row
+	y = current_y
+	link_1_d = current_link_1_d
+	while row < ROWS:
+		map_red_field.append([])
+		col = current_col
+		x = current_x
+		link_2_d = current_link_2_d
+		while col < COLS:
+			map_red_field[row][col].append(Point(x, y, link_1_d, link_2_d))
+			x += 51
+			if col == 0:
+				link_2_d += 175
+			else:
+				link_2_d += 150
+			col += 1
+		y += 65
+		link_1_d += 195
+		row += 1
+
+
+def update_current_state(row, col):
+	current_row = map_red_field[row][col].row
+	current_col = map_red_field[row][col].col
+	current_x = map_red_field[row][col].x
+	current_y = map_red_field[row][col].y
+	current_link_1_d = map_red_field[row][col].link_1_d
+	current_link_2_d = map_red_field[row][col].link_2_d
+
+
+def move_to(row, col):
+	if current_row != row:
+		move_link_2_d_to(row, 0)
+		move_link_1_d_to(row, col)
+	if current_col != col:
+		move_link_2_d_to(row, col)
+
+
+def move_link_1_d_to(row, col):
+	run_motor(MOTOR_D, map_red_field[row][col].link_1_d)
+	update_current_state(row, col)
+
+
+def move_link_2_d_to(row, col):
+	run_motor(MOTOR_A, map_red_field[row][col].link_2_d)
+	update_current_state(row, col)
 
 
 def run_motor(motor, angle):
-	motor.run_target(speed, angle)
-	# motor.reset_angle(-1) Сместилось на 20 градусов по обоим двигателям
-	# wait(10000)
+	motor.run_target(SPEED, angle)
 	wait(2000)
 
 
